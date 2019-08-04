94be939c49b9cf8ed180babb8fb84b42f3f73f2d
mohammad <m.jafari9877@gmail.com>
2019-08-04 15:18:47 +0430

    Add register controller. Implement get & post request APIs in controllers.

--- src/main/java/rahnema/tumaj/bid/backend/controllers/RegisterController.java
+++ src/main/java/rahnema/tumaj/bid/backend/controllers/RegisterController.java
@@ -1,23 +1,54 @@
 package rahnema.tumaj.bid.backend.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-import rahnema.tumaj.bid.backend.domains.UserInputDTO;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
+import org.springframework.web.bind.annotation.*;
+import rahnema.tumaj.bid.backend.domains.user.UserInputDTO;
+import rahnema.tumaj.bid.backend.domains.user.UserOutputDTO;
+import rahnema.tumaj.bid.backend.models.User;
+import rahnema.tumaj.bid.backend.services.UserService;
+import rahnema.tumaj.bid.backend.utils.assemblers.UserResourceAssembler;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
 public class RegisterController {
-    // TODO: Change the return type to <UserOutputDto>
-    @PostMapping(path = "/users")
-    public void addUser(UserInputDTO user) {
 
+    private final UserService userService;
+    private final UserResourceAssembler assembler;
+
+    public RegisterController(UserService userService,
+                              UserResourceAssembler assembler) {
+        this.userService = userService;
+        this.assembler = assembler;
     }
 
-    // TODO: Change the return type to <UserOutputDto>
     @GetMapping(path = "/users/{id}")
-    public void getUser(UserInputDTO user, @PathVariable String id) {
+    public Resource<UserOutputDTO> getOneUser(@PathVariable Long id) {
+        User user = userService.getOne(id).orElseThrow(EntityNotFoundException::new);
+        return assembler.toResource(user);
+    }
 
+    @GetMapping(path = "/users")
+    public Resources<Resource<UserOutputDTO>> getAllUsers() {
+        List<Resource<UserOutputDTO>> users = userService.getAll().stream()
+                .map(assembler::toResource)
+                .collect(Collectors.toList());
+        return new Resources<>(
+            users,
+            linkTo(methodOn(RegisterController.class).getAllUsers()).withSelfRel()
+        );
+    }
+
+    @PostMapping(path = "/users")
+    public Resource<UserOutputDTO> addUser(@RequestBody UserInputDTO user) {
+        this.userService.addOne(user);
+        return assembler.toResource(UserInputDTO.toModel(user));
     }
 
 }
