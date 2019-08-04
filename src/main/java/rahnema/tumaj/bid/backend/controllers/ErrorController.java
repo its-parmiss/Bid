295f134d9944297f1c31e241790cea0d7ca2020b
Amirali Monjar <monjaramirali@gmail.com>
2019-08-04 15:59:39 +0430

    Auctions Exception Handled!

--- src/main/java/rahnema/tumaj/bid/backend/controllers/ErrorController.java
+++ src/main/java/rahnema/tumaj/bid/backend/controllers/ErrorController.java
@@ -1,4 +1,23 @@
 package rahnema.tumaj.bid.backend.controllers;
 
-public class ErrorController {
+import com.google.gson.Gson;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import rahnema.tumaj.bid.backend.utils.exceptions.AuctionNotFoundException;
+import rahnema.tumaj.bid.backend.utils.exceptions.ExceptionMessage;
+
+@ControllerAdvice
+public class ErrorController extends ResponseEntityExceptionHandler {
+
+    private Gson gson = new Gson();
+
+    @ExceptionHandler(value = {AuctionNotFoundException.class})
+    public ResponseEntity<Object> entityNotFound(RuntimeException ex, WebRequest request) {
+        return handleExceptionInternal(ex, gson.toJson(new ExceptionMessage(ex.getMessage(), 404)), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+    }
 }
