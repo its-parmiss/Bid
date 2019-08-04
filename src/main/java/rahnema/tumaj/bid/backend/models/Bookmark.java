3d9ac685fe6dd869a2b3d90cb6c404969b78c6e4
Parmiss Shahinfard <parmiss.sh98@gmail.com>
2019-08-04 12:31:45 +0430

    implement models except for bids and bookmarks - entity annotations and lombok

--- src/main/java/rahnema/tumaj/bid/backend/models/Bookmark.java
+++ src/main/java/rahnema/tumaj/bid/backend/models/Bookmark.java
@@ -1,20 +1,17 @@
-//package rahnema.tumaj.bid.backend.models;
-//
-//import javax.persistence.Column;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//import java.util.Date;??
-//        `id` int PRIMARY KEY,
-//        `user_id` int,
-//        `auction_id` int,
-//        `created_at` datetime DEFAULT (now())
-//??
-//public class Bookmark {
-//    @Id
-//    @GeneratedValue(strategy= GenerationType.AUTO)
-//    Long id;
-//    @Column(name = "created_at",columnDefinition="DATE DEFAULT CURRENT_DATE")
-//    Date created_at;
-//    @
-//}
+package rahnema.tumaj.bid.backend.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+@Entity
+@Table(name="Bookmarks")
+@Data
+public class Bookmark {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    Long id;
+    @Column(name = "created_at",columnDefinition="DATE DEFAULT CURRENT_DATE")
+    Date created_at;
+
+}
--- src/main/java/rahnema/tumaj/bid/backend/models/Category.java
+++ src/main/java/rahnema/tumaj/bid/backend/models/Category.java
@@ -1,10 +1,14 @@
 package rahnema.tumaj.bid.backend.models;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.util.Date;
 import java.util.Set;
 
-
+@Entity
+@Table(name="Categories")
+@Data
 public class Category {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
