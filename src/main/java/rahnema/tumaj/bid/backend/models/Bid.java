b9bdb6eb992b64cb4f18e57b95f875fa97899f17
Parmiss Shahinfard <parmiss.sh98@gmail.com>
2019-08-04 13:32:58 +0430

    models design finished

--- src/main/java/rahnema/tumaj/bid/backend/models/Bid.java
+++ src/main/java/rahnema/tumaj/bid/backend/models/Bid.java
@@ -4,14 +4,20 @@ import lombok.Data;
 
 import javax.persistence.*;
 import java.util.Date;
+
 @Entity
-@Table(name="Bookmarks")
+@Table(name="Bids")
 @Data
-public class Bookmark {
+public class Bid {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
     Long id;
+    @Column(nullable = false)
+    String title;
     @Column(name = "created_at",columnDefinition="DATE DEFAULT CURRENT_DATE")
     Date created_at;
-
+    @ManyToOne
+    Auction related_auction;
+    @ManyToOne
+    User bidder;
 }
