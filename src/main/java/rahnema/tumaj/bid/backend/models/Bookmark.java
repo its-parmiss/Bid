b3a177e8d341573cdcab7fcef5306fcb8bda9870
Parmiss Shahinfard <parmiss.sh98@gmail.com>
2019-08-04 12:19:33 +0430

    implement models except for bids and bookmarks

---
+++ src/main/java/rahnema/tumaj/bid/backend/models/Bookmark.java
@@ -0,0 +1,20 @@
+//package rahnema.tumaj.bid.backend.models;
+//
+//import javax.persistence.Column;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import java.util.Date;??
+//        `id` int PRIMARY KEY,
+//        `user_id` int,
+//        `auction_id` int,
+//        `created_at` datetime DEFAULT (now())
+//??
+//public class Bookmark {
+//    @Id
+//    @GeneratedValue(strategy= GenerationType.AUTO)
+//    Long id;
+//    @Column(name = "created_at",columnDefinition="DATE DEFAULT CURRENT_DATE")
+//    Date created_at;
+//    @
+//}
