342cf1de94a58289b02e0da723287f79e312dbe4
Amirali Monjar <monjaramirali@gmail.com>
2019-08-14 13:59:32 +0430

    profile dto changed

--- src/main/java/rahnema/tumaj/bid/backend/controllers/UserController.java
+++ src/main/java/rahnema/tumaj/bid/backend/controllers/UserController.java
@@ -6,9 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import rahnema.tumaj.bid.backend.domains.auction.AuctionOutputDTO;
+import rahnema.tumaj.bid.backend.domains.user.ProfileOutputDTO;
+import rahnema.tumaj.bid.backend.domains.user.UserOutputDTO;
 import rahnema.tumaj.bid.backend.models.User;
 import rahnema.tumaj.bid.backend.services.Bookmarks.BookmarksService;
 import rahnema.tumaj.bid.backend.services.user.UserService;
+import rahnema.tumaj.bid.backend.utils.assemblers.UserResourceAssembler;
 import rahnema.tumaj.bid.backend.utils.athentication.TokenUtil;
 import rahnema.tumaj.bid.backend.utils.assemblers.AuctionAssemler;
 import rahnema.tumaj.bid.backend.utils.exceptions.NotFoundExceptions.TokenNotFoundException;
@@ -23,30 +26,54 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 @RestController
 public class UserController {
     private final UserService userService;
-    private final AuctionAssemler assembler;
+    private final AuctionAssemler auctionAssemler;
+    private final UserResourceAssembler userAssembler;
     private final BookmarksService bookmarksService;
 
     public UserController(UserService userService,
-                          AuctionAssemler assembler,
+                          AuctionAssemler auctionAssemler,
                           BookmarksService bookmarksService,
-                          TokenUtil tokenUtil) {
+                          TokenUtil tokenUtil,
+                          UserResourceAssembler userAssembler) {
         this.userService = userService;
-        this.assembler = assembler;
+        this.auctionAssemler = auctionAssemler;
         this.bookmarksService = bookmarksService;
+        this.userAssembler = userAssembler;
     }
 
-    @GetMapping("/user/my-auctions")
+    @GetMapping("/profile")
 
-    public Resources<Resource<AuctionOutputDTO>> getAllAuctions(@RequestHeader("Authorization") String token) {
+    public Resource<ProfileOutputDTO> getProfile(@RequestHeader("Authorization") String token) {
+        ProfileOutputDTO outputDTO = new ProfileOutputDTO();
+        extractFieldsAndSetProfile(token, outputDTO);
+        return getProfileOutputDTOResource(token, outputDTO);
+    }
+
+    private void extractFieldsAndSetProfile(@RequestHeader("Authorization") String token, ProfileOutputDTO outputDTO) {
         User user = userService.getUserWithToken(token);
+        List<Resource<AuctionOutputDTO>> myAuctions = collectMyAuctions(user);
+        List<Resource<AuctionOutputDTO>> bookmarked = collectAllBookmarks(user);
 
-        List<Resource<AuctionOutputDTO>> auctions = user.getMyAuctions().stream()
-                .map(assembler::assemble)
-                .collect(Collectors.toList());
-        return new Resources<>(
-                auctions,
-                linkTo(methodOn(AuctionController.class).getAll(null, null, null,null,null)).withSelfRel()
+        setProfileFields(user, outputDTO, myAuctions, bookmarked);
+    }
+
+    private Resource<ProfileOutputDTO> getProfileOutputDTOResource(@RequestHeader("Authorization") String token, ProfileOutputDTO outputDTO) {
+        return new Resource<>(
+                outputDTO,
+                linkTo(methodOn(UserController.class).getProfile(token)).withSelfRel()
         );
+    }
+
+    private void setProfileFields(User user, ProfileOutputDTO outputDTO, List<Resource<AuctionOutputDTO>> myAuctions, List<Resource<AuctionOutputDTO>> bookmarked) {
+        outputDTO.setMe( this.userAssembler.toResource(UserOutputDTO.fromModel(user)) );
+        outputDTO.setBookmarked(bookmarked);
+        outputDTO.setMyAuctions(myAuctions);
+    }
+
+    private List<Resource<AuctionOutputDTO>> collectMyAuctions(User user) {
+        return user.getMyAuctions().stream()
+                .map(this.auctionAssemler::assemble)
+                .collect(Collectors.toList());
     }
 
     @GetMapping("/user/bookmarked")
@@ -62,7 +89,7 @@ public class UserController {
 
     private List<Resource<AuctionOutputDTO>> collectAllBookmarks(User user) {
         return bookmarksService.getAll(user).stream()
-                .map(this.assembler::assemble)
+                .map(this.auctionAssemler::assemble)
                 .collect(Collectors.toList());
     }
 }
