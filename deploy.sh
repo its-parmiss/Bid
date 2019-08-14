97d9cefe3d854905f315e79012f4815f6203e769
mohammad <m.jafari9877@gmail.com>
2019-08-14 16:19:30 +0430

    deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 rm build/libs/bid-backend-service.jar
 ./gradlew build
 scp build/libs/bid-backend-service.jar team5@192.168.11.191:/home
+ssh team5@192.168.11.191 'java -jar ../bid-backend-service.jar'
