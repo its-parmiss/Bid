b0c6a0a3ac85982a120ff3dfb487b095cf20c855
mohammad <m.jafari9877@gmail.com>
2019-08-24 12:07:33 +0430

    Auto kill the pid before deployment

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 ./gradlew clean
 ./gradlew build
 scp build/libs/bid-backend-service.jar team5@192.168.11.191:/home
-ssh team5@192.168.11.191 'cd .. ; java -Dspring.config.location=./application.properties -jar bid-backend-service.jar'
+javaPid=$(ssh team5@192.168.11.191 "script/getJavaPid.sh")
+ssh team5@192.168.11.191 "kill $javaPid;cd ..;java -Dspring.config.location=./application.properties -jar bid-backend-service.jar"
