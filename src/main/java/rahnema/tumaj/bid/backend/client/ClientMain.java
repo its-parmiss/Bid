a17df5a0241917991c49a575cc307a7d5d2d6601
Amirali Monjar <monjaramirali@gmail.com>
2019-08-25 11:17:45 +0430

    authentication happend on websocket and refactor

--- src/main/java/rahnema/tumaj/bid/backend/client/ClientMain.java
+++ src/main/java/rahnema/tumaj/bid/backend/client/ClientMain.java
@@ -43,89 +43,55 @@ import rahnema.tumaj.bid.backend.domains.Messages.EnterAuctionMessage;
  * @Author Jay Sridhar
  */
 public class ClientMain {
-    static public class MyStompSessionHandler
-            extends StompSessionHandlerAdapter {
-        private String auctionId;
-
-        public MyStompSessionHandler(String auctionId) {
-            this.auctionId = auctionId;
-        }
-
-        private void showHeaders(StompHeaders headers) {
-            for (Map.Entry<String, List<String>> e : headers.entrySet()) {
-                System.err.print("  " + e.getKey() + ": ");
-                boolean first = true;
-                for (String v : e.getValue()) {
-                    if (!first) System.err.print(", ");
-                    System.err.print(v);
-                    first = false;
-                }
-                System.err.println();
-            }
-        }
 
 
-        @Override
-        public void afterConnected(StompSession session,
-                                   StompHeaders connectedHeaders) {
-            System.err.println("Connected! Headers:");
-            showHeaders(connectedHeaders);
+    final static String url = "ws://localhost:8080/test-websocket";
 
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        WebSocketStompClient stompClient = configClient();
+        String auctionId = in.readLine();
+        String token = in.readLine();
+        StompSession session = connectClient(stompClient, auctionId, token);
+        subscribe(auctionId, session);
+        session.send("/app/enter", new EnterAuctionInputMessage(auctionId));
+
+        while (!auctionId.equals("exit")) {
+            auctionId = in.readLine();
         }
     }
 
-    public static void main(String args[]) throws Exception {
-        WebSocketClient simpleWebSocketClient =
-                new StandardWebSocketClient();
-        List<Transport> transports = new ArrayList<>(1);
-        transports.add(new WebSocketTransport(simpleWebSocketClient));
+    private static void subscribe(String auctionId, StompSession session) {
+        session.subscribe("/auction/" + auctionId, new StompFrameHandler() {
+            @Override
+            public Type getPayloadType(StompHeaders headers) {
+                return EnterAuctionMessage.class;
+            }
 
+            @Override
+            public void handleFrame(StompHeaders headers,
+                                    Object payload) {
+                System.err.println(payload.toString());
+            }
+        });
+    }
 
-        BufferedReader in =
-                new BufferedReader(new InputStreamReader(System.in));
-
-        SockJsClient sockJsClient = new SockJsClient(transports);
-        WebSocketStompClient stompClient =
-                new WebSocketStompClient(sockJsClient);
-
-
-        stompClient.setMessageConverter(new MappingJackson2MessageConverter());
-
-        String auctionId = in.readLine();
-        String url = "ws://localhost:8080/test-websocket";
+    private static StompSession connectClient(WebSocketStompClient stompClient, String auctionId, String token) throws InterruptedException, java.util.concurrent.ExecutionException {
         StompSessionHandler sessionHandler = new MyStompSessionHandler(auctionId);
         WebSocketHttpHeaders connectHeaders = new WebSocketHttpHeaders();
-        connectHeaders.add("Authorization", "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhbWlyYWxpLm1uakBnbWFpbC5jb20iLCJleHAiOjE1NjY3MjA3NzcsImlhdCI6MTU2NjcwMjc3N30.1IY4V4insxuYDwg9tlzwM2WxZToORiD_mHMrQHx9-L3AZUXbPg4s3yo3tO1FpFNbGGdE03blRgOWlsXVeswPFA");
-        StompSession session = stompClient.connect(url, connectHeaders, sessionHandler)
+        connectHeaders.add("Authorization", "Bearer " + token);
+        return stompClient.connect(url, connectHeaders, sessionHandler)
                 .get();
+    }
 
-
-        synchronized (session) {
-            session.subscribe("/auction/" + auctionId, new StompFrameHandler() {
-
-                @Override
-                public Type getPayloadType(StompHeaders headers) {
-                    return EnterAuctionMessage.class;
-                }
-
-                @Override
-                public void handleFrame(StompHeaders headers,
-                                        Object payload) {
-                    System.err.println(payload.toString());
-                }
-            });
-        }
-
-        session.send("/app/enter", new EnterAuctionInputMessage(auctionId));
-        Thread.sleep(500000);
-        /*for (;;) {
-            System.out.print(userId + " >> ");
-            System.out.flush();
-            String line = in.readLine();
-            if ( line == null ) break;
-            if ( line.length() == 0 ) continue;
-            ClientMessage msg = new ClientMessage(userId, line);
-            session.send("/app/chat/java", msg);
-        }*/
+    private static WebSocketStompClient configClient() {
+        WebSocketClient simpleWebSocketClient = new StandardWebSocketClient();
+        List<Transport> transports = new ArrayList<>(1);
+        transports.add(new WebSocketTransport(simpleWebSocketClient));
+        SockJsClient sockJsClient = new SockJsClient(transports);
+        WebSocketStompClient stompClient = new WebSocketStompClient(sockJsClient);
+        stompClient.setMessageConverter(new MappingJackson2MessageConverter());
+        return stompClient;
     }
 }
